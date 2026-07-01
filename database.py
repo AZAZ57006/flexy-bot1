28b21b5b9870a201bab0ef52ce3d83675f66ab36
DJAMEL LOUAFI <36001463+AZAZ57006@users.noreply.github.com>
2026-07-01 08:26:29 +0100

    Delete database.py

--- database.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-def init_db():
-conn = sqlite3.connect('history.db')
-cursor = conn.cursor()
-cursor.execute('''CREATE TABLE IF NOT EXISTS history
-(id INTEGER PRIMARY KEY, phone TEXT, amount TEXT, status TEXT)''')
-conn.commit()
-conn.close()
